2fcad5cd9099318b476e94a55544897f4594811f
Dimka <manager@perilman.com.ua>
2017-10-24 16:24:25 +0300

    add autoregistrator

--- registration.php
+++ registration.php
@@ -159,92 +159,258 @@ $error=[];
 //$place=(string)$place;
 
 
-
-function name(){
-    $name='';
-    for($i=0;$i<=20;$i++){
-        $name=
+class name
+{
+    public function Autoname()
+    {
+        
+        $chars = 'abdefhiknrstyzABDEFGHKNQRSTYZ';
+        $generate_string = '';
+        $length = rand(3, 20);
+        
+        for ($x = 0; $x < $length; $x++) {
+            $i = rand(0, 28);
+            $generate_string .= $chars{$i};
+        };
+        
+        return $generate_string;
     }
-    return $name;
-}
-
-if(preg_match('/^[\wа-яёії]{2,20}/iu', $name)===1){
-    $userData['name']=$name;
-}else{
-    $userData['name']='ОШИБКА';
-    $error['name']='Не корректное имя';
-}
-
-
-if(preg_match('/^[\wа-яёії]{2,20}/iu', $seurname)===1){
-    $userData['seurname']=$seurname;
-}else{
-    $userData['seurname']='ОШИБКА';
-    $error['seurname']='Не корректное отчество';
-}
-
-
-if(preg_match('/^0|1$/', $sex)===1){
-    $userData['sex']=$sex;
-}else{
-    $userData['sex']='ОШИБКА';
-    $error['sex']='Не корректный пол';
-}
-
-
-if(preg_match('/^[\wа-яёії0-9]{2,5}$/iu', $mygroup)===1){
-    $userData['mygroup']=$mygroup;
-}else{
-    $userData['mygroup']='ОШИБКА';
-    $error['mygroup']='Не корректная группа';
-}
-
-
-if(preg_match('/^([a-z0-9_\.-]+)@([a-z0-9_\.-]+)\.([a-z\.]{2,6})$/i', $email)===1){
-    $userData['email']=$email;
-}else{
-    $userData['email']='ОШИБКА';
-    $error['email']='Не корректная почта';
-}
-
-
-if(preg_match('/^[0-9]{1,4}$/', $balls)===1){
-    $userData['balls']=$balls;
-}else{
-    $userData['balls']='ОШИБКА';
-    $error['balls']='Не корректные быллы';
-}
-
-
-if(preg_match('/^[0-9]{4}$/', $berd_year)===1){
-    $userData['berd_year']=$berd_year;
-}else{
-    $userData['berd_year']='ОШИБКА';
-    $error['berd_year']='Не корректный год рождения';
-}
-
-
-if(preg_match('/^[\wа-яёії]{2,20}/iu', $place)===1){
-    $userData['place']=$place;
-}else{
-    $userData['place']='ОШИБКА';
-    $error['place']='Не корректный город';
-}
-
-
-if (in_array('ОШИБКА', $userData,true)){
-    echo json_encode($error);
-}else{
     
-    $user=new Addusers($userData);
-    $result=$user->save();
+}
+
+class seurname
+{
+    public function Seurname()
+    {
+        
+        $chars = 'abdefhiknrstyzABDEFGHKNQRSTYZ';
+        $generate_string = '';
+        $length = rand(3, 20);
+        
+        for ($x = 0; $x < $length; $x++) {
+            $i = rand(0, 28);
+            $generate_string .= $chars{$i};
+        };
+        
+        return $generate_string;
+    }
     
+}
+
+class sex
+{
+    public function Sex()
+    {
+        
+        $generate_string = rand(0, 1);
+        
+        return $generate_string;
+    }
     
+}
+
+class mygroup
+{
+    public function Mygroup()
+    {
+        
+        $chars = 'abdefhiknrstyzABDEFGHKNQRSTYZ1234567890';
+        $generate_string = '';
+        $length = rand(3, 5);
+        
+        for ($x = 0; $x < $length; $x++) {
+            $i = rand(0, 38);
+            $generate_string .= $chars{$i};
+        };
+        
+        return $generate_string;
+    }
+    
+}
+
+class email
+{
+    public function Email()
+    {
+        
+        $chars = 'abdefhiknrstyzABDEFGHKNQRSTYZ1234567890';
+        $chars2 = 'abdefhiknrstyz';
+        $generate_string = '';
+        $generate_string1 = '';
+        $generate_string2 = '';
+        $generate_string3 = '';
+        
+        $length = rand(3, 10);
+        $length2 = rand(2, 6);
+        
+        for ($x = 0; $x < $length; $x++) {
+            $i = rand(0, 38);
+            $generate_string1 .= $chars{$i};
+        };
+        
+        for ($i = 0; $i < $length; $i++) {
+            $a = rand(0, 38);
+            $generate_string2 .= $chars{$a};
+        };
+        
+        for ($y = 0; $y < $length2; $y++) {
+            $a = rand(0, 12);
+            $generate_string3 .= $chars2{$a};
+        };
+        
+        return $generate_string=$generate_string1.'@'.$generate_string2.'.'.$generate_string3;
+    }
+    
+}
+
+class balls
+{
+    public function Balls()
+    {
+        
+        $chars = '1234567890';
+        $generate_string = '';
+        $length = rand(1, 4);
+        
+        for ($x = 0; $x < $length; $x++) {
+            $i = rand(0, 9);
+            $generate_string .= $chars{$i};
+        };
+        
+        return $generate_string;
+    }
+}
+
+class year
+{
+    public function Year()
+    {
+        
+        $chars = '1234567890';
+        $generate_string = '';
+        $length = rand(4,4);
+        
+        for ($x = 0; $x < $length; $x++) {
+            $i = rand(0, 9);
+            $generate_string .= $chars{$i};
+        };
+        
+        return $generate_string;
+    }
+}
+
+class place
+{
+    public function Place()
+    {
+        
+        $chars = 'abdefhiknrstyzABDEFGHKNQRSTYZ';
+        $generate_string = '';
+        $length = rand(2, 19);
+        
+        for ($x = 0; $x < $length; $x++) {
+            $i = rand(0, 28);
+            $generate_string .= $chars{$i};
+        };
+        
+        return $generate_string;
+    }
+    
+}
+
+$schetcik=0;
+do {
+    for ($ay = 0; $ay < 100; $ay++) {
+        
+        
+        $name = (new name())->Autoname();
+        $seurname = (new seurname())->Seurname();
+        $sex = (new sex())->Sex();
+        $mygroup = (new mygroup())->Mygroup();
+        $email = (new email())->Email();
+        $balls = (new balls())->Balls();
+        $berd_year = (new year())->Year();
+        $place = (new place())->Place();
+        
+        
+        if (preg_match('/^[\wа-яёії]{2,20}/iu', $name) === 1) {
+            $userData['name'] = $name;
+        } else {
+            $userData['name'] = 'ОШИБКА';
+            $error['name'] = 'Не корректное имя';
+        }
+        
+        
+        if (preg_match('/^[\wа-яёії]{2,20}/iu', $seurname) === 1) {
+            $userData['seurname'] = $seurname;
+        } else {
+            $userData['seurname'] = 'ОШИБКА';
+            $error['seurname'] = 'Не корректное отчество';
+        }
+        
+        
+        if (preg_match('/^0|1$/', $sex) === 1) {
+            $userData['sex'] = $sex;
+        } else {
+            $userData['sex'] = 'ОШИБКА';
+            $error['sex'] = 'Не корректный пол';
+        }
+        
+        
+        if (preg_match('/^[\wа-яёії0-9]{2,5}$/iu', $mygroup) === 1) {
+            $userData['mygroup'] = $mygroup;
+        } else {
+            $userData['mygroup'] = 'ОШИБКА';
+            $error['mygroup'] = 'Не корректная группа';
+        }
+        
+        
+        if (preg_match('/^([a-z0-9_\.-]+)@([a-z0-9_\.-]+)\.([a-z\.]{2,6})$/i', $email) === 1) {
+            $userData['email'] = $email;
+        } else {
+            $userData['email'] = 'ОШИБКА';
+            $error['email'] = 'Не корректная почта';
+        }
+        
+        
+        if (preg_match('/^[0-9]{1,4}$/', $balls) === 1) {
+            $userData['balls'] = $balls;
+        } else {
+            $userData['balls'] = 'ОШИБКА';
+            $error['balls'] = 'Не корректные быллы';
+        }
+        
+        
+        if (preg_match('/^[0-9]{4}$/', $berd_year) === 1) {
+            $userData['berd_year'] = $berd_year;
+        } else {
+            $userData['berd_year'] = 'ОШИБКА';
+            $error['berd_year'] = 'Не корректный год рождения';
+        }
+        
+        
+        if (preg_match('/^[\wа-яёії]{2,20}/iu', $place) === 1) {
+            $userData['place'] = $place;
+        } else {
+            $userData['place'] = 'ОШИБКА';
+            $error['place'] = 'Не корректный город';
+        }
+        
+        
+        if (in_array('ОШИБКА', $userData, true)) {
+            echo json_encode($error);
+        } else {
+            
+            $user = new Addusers($userData);
+            $result = $user->save();
 //    if($result==true){
 //        $_SESSION['userdata']=serialize($user);
 //        setcookie('sit',session_id(),strtotime('10 year'));
 //        header('location: ' . SITE);
 //    }
-    
-};
-
+            
+        };
+        
+    };
+    $schetcik++;
+}while($schetcik<5);
