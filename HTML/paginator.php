906c6ba74b662f2f6a395df3990de5814e925188
Dimka <manager@perilman.com.ua>
2017-10-26 02:20:08 +0300

    cool Yea

--- HTML/paginator.php
+++ HTML/paginator.php
@@ -2,12 +2,10 @@
     <div class="navbar text-center ">
         <div id="page">
             <ul class="navbar-fixed-bottom paginator">
-            <li>
                 <button id="page2left"  class="btn btn-sm btn-danger" ><<</button>
                 <button id="page1left"  class="btn btn-sm btn-danger" ><</button>
                 <button id="page1reigth"  class="btn btn-sm btn-danger" >></button>
                 <button id="page2reigth"  class="btn btn-sm btn-danger" >>></button>
-            </li>
             </ul>
         </div>
     </div>
