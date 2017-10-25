730d2dc80af27a1633b351475391b19afdcc005f
Dimka <manager@perilman.com.ua>
2017-10-26 02:23:57 +0300

    delete bad html

--- HTML/paginator.php
+++ HTML/paginator.php
@@ -11,8 +11,3 @@
     </div>
 </div>
 
-<!--два файла один лежит в /data.php-->
-<!--второй файл лежит в /HTML/paginator.php-->
-<!--между ними не передаются $_GET запросы.-->
-<!--Если подключить data.php  realpath(__DIR__).'/data.php' в файл /HTML/paginator.php то запросы проходят.-->
-<!--Но отображаются echo json_encode($response); Как быть?-->
